a26ece062993940cacb75236e651010f6df8f0a1
Kali Linux <kali@ip-172-31-84-86.ec2.internal>
2022-08-25 19:05:30 +0000

    Committer: Kali Linux <kali@ip-172-31-84-86.ec2.internal>  Changes to be committed: 	modified:   package-lock.json 	modified:   set_env.sh 	modified:   udagram-api-feed/package-lock.json 	modified:   udagram-api-user/package-lock.json 	modified:   udagram-frontend/package-lock.json 	modified:   udagram-frontend/www/styles.js 	modified:   udagram-frontend/www/styles.js.map 	modified:   udagram-reverseproxy/nginx.conf

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=me
+export POSTGRES_USERNAME=d1
 export POSTGRES_PASSWORD=rdsdbpassword
-export POSTGRES_HOST=database1.cguijthxfahu.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=database1
-export AWS_BUCKET=arn:aws:s3:::outgam
+export POSTGRES_HOST=d1.cguijthxfahu.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=d1
+export AWS_BUCKET=arn:aws:s3:::outgtam
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
