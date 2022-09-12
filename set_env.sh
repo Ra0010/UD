ba32cce138ef9229e2e7eb8e34eaa7fd7c88ad87
Ahmed H <91718675+Ra0010@users.noreply.github.com>
2022-09-12 23:38:10 +0100

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=d11
+export POSTGRES_USERNAME=a1
 export POSTGRES_PASSWORD=rdsdbpassword
-export POSTGRES_HOST=d11.canmmrf457au.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=d11
-export AWS_BUCKET=arn:aws:s3:::udagram00
+export POSTGRES_HOST=a1.c2e7uvfqo80i.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=a1
+export AWS_BUCKET=arn:aws:s3:::udagram123
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
